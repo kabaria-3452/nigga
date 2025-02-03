28d81e3b053215fe0c00a9c6289af530693dedd2
kabaria-3452 <m62131193@gmail.com>
2025-02-03 16:32:25 +0300

    initial commit

---
+++ intoduction.py
@@ -0,0 +1,8 @@
+#print is a method used to display an output
+
+
+
+print("Frankline")
+print("Emobilis")
+print(100)
+print(10+20)
---
+++ variable.py
@@ -0,0 +1,23 @@
+# variable is a container used to store a value
+
+location ="Westlands"
+firstname ="Frankline"
+lastname = "emobilis"
+school ="emobilis"
+
+
+print(location)
+print(firstname)
+print(lastname)
+print(school)
+x =39
+y =2
+
+
+print(firstname,lastname)
+print("my fullname is",firstname,lastname)
+print(school,"emobilis is a coding school")
+print( x + y)
+x =39
+y =2
+#The product of 39 and 2 is 78
